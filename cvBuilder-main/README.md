4542e57f2bb1eedbeec16f74985c22e501a8ae67
Saurabh Sushir <sushirss1@gmail.com>
2024-12-12 15:11:13 +0530

    final

---
+++ cvBuilder-main/README.md
@@ -0,0 +1,74 @@
+# Modern Resume/CV Generator Application
+
+Create stunning, modern resumes effortlessly with our Next.js-based CV generator. Our application is designed to help you craft minimalist, beautiful resumes in three distinct templates, exportable in PDF format. It utilizes the power of Tailwind CSS for styling and leverages the Context API for efficient state management.
+
+![Sample Resume](sample-resume.png)
+
+## Table of Contents
+
+- [Demo](#demo)
+- [Features](#features)
+- [Getting Started](#getting-started)
+- [How to Use](#how-to-use)
+- [Templates](#templates)
+- [Contributing](#contributing)
+
+## Demo
+
+You can experience our CV generator live by visiting the [Demo](https://cvbuilder.gauravxshukla.me/) page.
+
+## Features
+
+- Create modern, minimalist resumes effortlessly.
+- Choose from three beautiful template designs.
+- Export your resume in PDF format.
+- Responsive design for all screen sizes.
+- Stylish, mobile-friendly UI powered by Tailwind CSS.
+- Efficient state management with the Context API.
+
+## Getting Started
+
+To run this application locally on your machine, follow these steps:
+
+1. Clone this repository to your local machine using Git:
+
+   ```bash
+   git clone https://github.com/gauravvv-shukla/cvBuilder.git
+   ```
+
+2. Install the dependencies:
+
+   ```bash
+   cd cvBuilder
+   npm install
+   ```
+
+3. Start the development server:
+
+   ```bash
+   npm run dev
+   ```
+
+4. Open your browser and navigate to [http://localhost:3000](http://localhost:3000) to access the CV generator.
+
+## How to Use
+
+1. Choose a template that suits your style and preferences.
+2. Fill in your personal information, including your name, contact details, and professional summary.
+3. Add your education, work experience, and any additional sections you require.
+4. Preview your resume to see how it looks in real-time.
+5. Export your resume as a PDF file for easy sharing and printing.
+
+## Templates
+
+Our CV generator offers three modern and minimalist templates to choose from, allowing you to create a personalized and visually appealing resume that suits your needs.
+
+![Templates](templates.png)
+
+## Contributing
+
+Contributions to this project are welcome. If you have suggestions, bug reports, or feature requests, please open an issue on the GitHub repository. If you'd like to contribute code, fork the repository, create a feature branch, and submit a pull request.
+
+---
+
+Craft beautiful, modern resumes effortlessly with our Next.js-powered CV generator. If you have any questions or need assistance, please don't hesitate to [contact us](mailto:gauravvvshukla@gmail.com).
---
+++ cvBuilder-main/data/cvData.js
@@ -0,0 +1,129 @@
+export const cvData = {
+  name: "Gaurav Shukla",
+  image: "",
+  jobTitle: "Developer, Designer",
+  location: "Nagpur, India",
+  email: "gauravxdesign@gmail.com",
+  linkedIn: "",
+  instagram: "",
+  facebook: "",
+  twitter: "gauravv_shukla",
+  github: "gauravvv-shukla",
+  website: "thegauravshukla.me",
+  about:
+    "Since my sophomore year during my Engineering, I used to be a Graphic Designer for 3 years.After that, I traversed into Designing and Frontend Development due to my past work. Because of my old job, *I give importance to design and visuality in my work.*",
+
+  toolsAndTechSkills: [
+    "Photoshop",
+    "Figma",
+    "Premiere Pro",
+    "After Effects",
+    "Webflow",
+    "Framer",
+    "Illustrator",
+  ],
+  industryKnowledge: [
+    "Javascript",
+    "CSS",
+    "HTML",
+    "Next.js",
+    "React",
+    "Tailwind",
+  ],
+  languages: ["English", "Hindi"],
+  skillTitle1: "Tools and Tech Skills",
+  skillTitle2: "Industry Knowledge",
+  skillTitle3: "Languages",
+  projects: [
+    {
+      title: "Promptopia",
+      link: "https://prompts.thegauravshukla.me/",
+      summary: `Created a prompt sharing system wherein you get creative prompt for your ChatGPT problems.`,
+    },
+    {
+      title: "Linear Homepage Remake",
+      link: "https://linear.thegauravshukla.me/",
+      summary: `Redesigned existing Linear website using Next.js,Tailwind and Framer motion`,
+    },
+
+    {
+      title: "weatherWatch",
+      link: "https://weatherwatchh.vercel.app//",
+      summary: `In this work, a small project where I visualize the data from the API using React, CSS, Chart.js, GeolocationAPI and WeatherAPI`,
+    },
+  ],
+  education: [
+    {
+      title: "Bachelor of Engineering in Computer Science",
+      school: "Shri Ramdeobaba College Of Engineering and Management, Nagpur",
+      startDate: "2018",
+      endDate: "2022",
+    },
+  ],
+  experiences: [
+    {
+      title: "Software Engineer",
+      company: "Accenture",
+      startDate: "2023",
+      endDate: "Present",
+      current: true,
+      summary: `Built and deployed multiple RESTful APIs using Node.js and Express, reducing API development time by 40%.
+      Implemented SSL/TLS encryption improving data privacy and reducing the risk of data breaches by over 80%.
+      Utilized Route 53 which improved response times by 60%.
+      Tech Stack - Node, Express, Amazon Web Services.`,
+    },
+    {
+      title: "Software Engineer",
+      company: "BrilliantRev",
+      startDate: "2022",
+      endDate: "2023",
+      current: true,
+      summary: `Built cross-browser compatible and responsive designs, improving the user experience across devices by 50%. Utilized CSS preprocessors such as SASS to write modular CSS code, reducing development time by 30%. Implemented responsive images using CSS and JavaScript techniques, reducing image loading times by 300 millisecs. Tech Stack - React, Redux, Axios, Bootstrap, MaterialUI.`,
+    },
+  ],
+  displayImage: true,
+  displayMail: true,
+  displayWebSite: true,
+  displayLinkedIn: false,
+  displayInstagram: false,
+  displayFacebook: false,
+  displayGithub: true,
+  displayTwitter: true,
+  activeColor: "#5B21B6",
+};
+
+/*
+export const cvDataEmpty = {
+  name: "",
+  jobTitle: "",
+  location: "",
+  email: "",
+  linkedin: "",
+  twitter: "",
+  github: "",
+  website: "",
+  aboutme: "",
+  toolsAndTechSkills: [],
+  industryKnowledge: [],
+  languages: [],
+  projects: [
+    {
+      title: "",
+      summary: "",
+    },
+  ],
+  experience: [
+    {
+      title: "",
+      company: "",
+      from: new Date(),
+      to: new Date(),
+      current: true,
+      summary: "",
+    },
+  ],
+  displayEducation: false,
+  displayProjects: false,
+  activeColor: "#5B21B6",
+};
+*/
